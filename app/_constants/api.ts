66464a90488b419b258474ced230d9343b7df87f
syamcat <kdh1998kdh@gmail.com>
2025-09-12 14:36:54 +0900

    signup url

--- app/_constants/api.ts
+++ app/_constants/api.ts
@@ -3,7 +3,7 @@ export const API_BASE_URL = "http://localhost:9988";
 
 // API Endpoints
 export const API_ENDPOINTS = {
-	SIGNUP: "/signup",
+	SIGNUP: "/member/signup",
 	LOGIN: "/login",
 	LOGOUT: "/logout",
 } as const;
